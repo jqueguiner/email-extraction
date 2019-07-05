c7333463a785bc999657a19f50cbac6161ebe77c
jqueguin <jqueguin@macovh-1.home>
2019-07-05 07:00:15 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# email-extractor
+# Docker for API
+
+You can build and run the docker using the following process:
+
+Cloning
+```console
+gti clone https://github.com/jqueguiner/XXXX.git XXXXX
+```
+
+Building Docker
+```console
+cd XXXXX && docker build -t XXXXX -f Dockerfile .
+```
+
+Running Docker
+```console
+echo "http://$(curl ifconfig.io):5000" && docker run -p 5000:5000 -d XXXXX
+```
+
+Calling the API
+```console
+curl -X POST "http://MY_SUPER_API_IP:5000/process" -H "accept: application/json" -H "Content-Type: application/json" -d '{"url":"https://i.ibb.co/FqDSQF8/input.jpg"}'
+```
+
+Calling the API
+```console
+curl -X POST "http://MY_SUPER_API_IP:5000/detect" -H "accept: application/json" -H "Content-Type: application/json" -d '{"url":"https://i.ibb.co/FqDSQF8/input.jpg"}'
+```
---
+++ src/app.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import subprocess
+import requests
+import ssl
+import random
+import string
+import json
+
+from flask import jsonify
+from flask import Flask
+from flask import request
+from flask import send_file
+import traceback
+
+from app_utils import blur
+from app_utils import download
+from app_utils import generate_random_filename
+from app_utils import clean_me
+from app_utils import clean_all
+from app_utils import create_directory
+from app_utils import get_model_bin
+from app_utils import get_multi_model_bin
+
+import re
+
+try:  # Python 3.5+
+    from http import HTTPStatus
+except ImportError:
+    try:  # Python 3
+        from http import client as HTTPStatus
+    except ImportError:  # Python 2
+        import httplib as HTTPStatus
+
+
+app = Flask(__name__)
+
+
+@app.route("/detect", methods=["POST"])
+def process():
+
+    try:
+        text = request.json["text"]
+   
+        results = []
+
+        matches = regex.finditer(text)
+
+        for matchNum, match in enumerate(matches, start=1):
+    
+            out = "Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group())
+            results.append({'verbose': out, 'start': match.start(), 'matchNum': matchNum,'end': match.end(), 'match': match.group()})
+    
+        return json.dumps(results), 200
+
+    except:
+        traceback.print_exc()
+        return {'message': 'input error'}, 400
+
+
+
+if __name__ == '__main__':
+    global regex
+
+    regex = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]', re.IGNORECASE|re.MULTILINE)
+    #mail_regex = re.compile(r'[\w\.-]+@[\w\.-]+')
+
+
+    port = 5000
+    host = '0.0.0.0'
+
+    app.run(host=host, port=port, threaded=True)
+
